36a8311e2c486c5079594a965a0df5a509dae234
Dima Shirokov <dshirokoff@ukr.net>
2016-09-16 16:41:24 +0300

    Instagram.getUsersRecentMedia

---
+++ api/post_getUsersRecentMedia.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+module.exports = (req, res) => {
+
+    let { accessToken, userId="self", count, maxId, minId, to="to" } = req.body.args;
+
+    let r = {
+        callback        : "",
+        contextWrites   : {}
+    };
+
+    if(!accessToken || !userId) {
+        _.echoBadEnd(r, to, res);
+        return;
+    }
+
+    let uri = `https://api.instagram.com/v1/users/${userId}/media/recent/?access_token=${accessToken}`;
+    
+    if(count) uri += `&count=${count}`;
+    if(maxId) uri += `&max_id=${maxId}`;
+    if(minId) uri += `&min_id=${minId}`;
+
+    return request(uri, (err, response, body) => {
+    	if(!err && response.statusCode == 200) {
+            console.log(JSON.stringify(body))
+    		r.contextWrites[to] = JSON.stringify(body);
+            r.callback = 'success'; 
+        } else {
+            r.contextWrites[to] = err;
+            r.callback = 'error';
+        }
+
+        res.status(200).send(r);
+    });
+};
